437bde893b74ac5bd7d9feed2bc46e7284dae77a
abi <kazmatovabi800@gmail.com>
2020-10-22 15:14:24 +0800

    house

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import Header from "../header";
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import {Carousel} from 'react-responsive-carousel';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Header/>
+                <Carousel>
+                    <div>
+                        <img src="https://zastavok.net/main/macro/159709956593.jpg" alt=""/>
+                        <h2>sample caption</h2>
+                    </div>
 
+                    <div>
+                        <img src="https://zastavok.net/main/graphics/159707712948.jpg" alt=""/>
+                        <h2>sample caption</h2>
+                    </div>
 
+                    <div>
+                        <img src="https://zastavok.net/main/sport/159708444661.jpg" alt=""/>
+                        <h2>sample caption</h2>
+                    </div>
+                </Carousel>
             </div>
         );
     }
